31b66dbec3dca03ce5117b301fece9b588be804c
Wilton Ferreira <wilton46ferreira@gmail.com>
2026-09-17 18:34:10 -0300

    Final
    A parte requisitada foi concluida

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # jogoMario
 Criando e testando jogo do Mario para repositorio
+
+Aluno: WILTON FERREIRA COSTA NETO 
+Matricula: 01904543
+Curso: CIÊNCIA DA COMPUTAÇÃO
---
+++ projeto_jogoMario/js/script.js
@@ -0,0 +1,33 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+  if (mario.classList.contains('jump')) return;
+
+  mario.classList.add('jump');
+  setTimeout(() => {
+    mario.classList.remove('jump');
+  }, 500);
+};
+
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = parseFloat(window.getComputedStyle(mario).bottom);
+
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+    pipe.style.right = 'auto';
+
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
+    mario.src = './imagens/game-over.png';
+    mario.style.width = '75px';
+    mario.style.marginLeft = '50px';
+
+    clearInterval(loop);
+  }
+}, 10);
+
+document.addEventListener('keydown', jump);
+document.addEventListener('click', jump);
